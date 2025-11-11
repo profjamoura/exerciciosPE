56e0f27d13f538906c385b619ff6fe63f6595314
Prof J A Moura <profjamoura@gmail.com>
2025-11-11 16:06:28 -0300

    Add calculaAltura class for building height calculation

---
+++ calculaAltura.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class calculaAltura {
+    public static void calcula() {
+        Scanner tecla = new Scanner(System.in);
+        int angulo = 0;
+        int catetoAdjacente = 0;
+        String nome = "";
+        do {
+            System.out.println("Calculo da altura do predio");
+            System.out.print("Digite o nome do predio ");
+            nome = tecla.nextLine();
+            System.out.print("Digite a distancia do predio ate a medicao do angulo em metros ");
+            catetoAdjacente = tecla.nextInt();
+            System.out.print("Digite o angulo de projecao ");
+            angulo = tecla.nextInt();
+        }
+        while ((angulo >= 90) || (angulo < 0));
+        double catetoOposto = Math.tan((Math.PI / 180) * angulo) * catetoAdjacente;
+        tecla.close();
+        System.out.println("O " + nome + " tem " + catetoOposto + " m");
+        System.out.println("tangente de " + angulo + " = " + Math.tan((Math.PI / 180) * angulo));
+    }
+}
+// (Math.PI/180) * angulo
