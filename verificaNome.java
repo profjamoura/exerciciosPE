8b7ab736eec66474bdf3cdf9608d7fad9f9efe8c
Prof J A Moura <profjamoura@gmail.com>
2025-11-11 11:05:30 -0300

    Add name verification feature
    Implement name verification to check for 'Neto' suffix

---
+++ verificaNome.java
@@ -0,0 +1,24 @@
+package org.example;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        String nome ="";
+        Boolean ultNome;
+        do {
+            System.out.println("Retira 'Neto' do nome");
+            System.out.println("Digite o nome completo ");
+
+            Scanner ler = new Scanner(System.in);
+            nome = ler.nextLine();
+            ultNome = nome.endsWith("neto");
+            System.out.println(ultNome);
+            if (ultNome) {
+                System.out.println(">>> Nome contem 'Neto'");
+            } else {
+                System.out.println("Nome nao contem 'Neto'");
+            }
+        }
+        while (!nome.equals("."));
+    }
+}
+
