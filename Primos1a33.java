d0f35a4f37915058c3e8c06ac83f4d3f1c852f8a
Prof J A Moura <profjamoura@gmail.com>
2025-12-19 16:07:37 -0300

    Modify prime number calculation and output format
    Update prime number logic to start from 8 and print results in a single line.

--- Primos1a33.java
+++ Primos1a33.java
@@ -3,15 +3,16 @@ package org.example;
 public class Primos1a33 {
     public static void main (String [ ] args){
         // Primos de 1 a 33
+        System.out.print("1 2 3 5 7 ");
 
-        for (int i = 1; i <= 33; i++){
+        for (int i = 8; i <= 33; i++){
             if (!(i % 2 == 0) &&
                     !(i % 3 == 0) &&
                     !(i % 5 == 0) &&
                     !(i % 7 == 0))
-                {
-                    System.out.println(i);
-                }
+            {
+                System.out.print(i + " ");
+            }
         }
     }
 }
